467b9865c778f22495802f55813986acbb1522b9
sinhahrtk <hrithiksinha321@gmail.com>
2025-09-20 18:14:45 +0530

    Training Day 01

--- src/T_02.java
+++ src/T_02.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class T_02 {
 
     public static void main(String[] args) {
-        System.out.println("Java Part 1");
+
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Input length value");
+        int length = sc.nextInt();
+        System.out.println("Length = " + length);
+
+        System.out.println("Input breadth value");
+        int breadth = sc.nextInt();
+        System.out.println("Breadth = " + breadth);
+
+        int Area = length*breadth ;
+        System.out.println("Area of Rectangle = " + Area);
+
     }
 
 
--- src/T_03.java
+++ src/T_03.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class T_03 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int r = sc.nextInt();
+        int t = sc.nextInt();
 
+        double s = p*r*t;
+        double si = s/100;
+
+        System.out.println("Simple Intrest = " + si);
+
+    }
 }
